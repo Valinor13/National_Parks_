05be682b70c8963df30c14def1a86bb16882a314
Valinor13 <jwcalhoun2@gmail.com>
2022-02-23 10:37:15 -0600

    Create data.js

---
+++ models/data.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Class object that communicates with mongodb database
+const DataScheme  = new mongoose.Schema({
+    title: {
+        type: String
+    },
+    data: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model("Data", DataScheme);
